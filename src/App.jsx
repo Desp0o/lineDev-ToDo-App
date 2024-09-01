0a1c1b832ef220d6fab6f9d732345bf8e1e468a2
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-01 23:54:12 +0400

    set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom"
+import SignIn from "./pages/SignIn"
 
 function App() {
 
   return (
-    <>
-      <p className="text-xl text-lime-500	">11</p>
-    </>
+    <Routes>
+      <Route path='/' element={<SignIn />} />
+    </Routes>
   )
 }
 
