2a81a434504f6ddb129b2ef83953d6e56b140310
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-04 16:10:09 +0400

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,12 @@ import Layout from "../components/Layout";
 import { PieChart } from "../components/PieChart";
 import FetchData from "../functions/FetchData";
 import InfoBlock from "../components/InfoBlock";
+import Spinner from "../components/Spinner";
 
 const Dashboard = () => {
-  const { data } = FetchData();
+  const { data, isLoading } = FetchData();
   const [completeLength, setCompleteLength] = useState(0);
-  const [inProgressLength, setInProgressLength] = useState(10);
+  const [inProgressLength, setInProgressLength] = useState(0);
   const [importantLength, setImportantLength] = useState(0);
 
   useEffect(() => {
@@ -16,7 +17,6 @@ const Dashboard = () => {
     setCompleteLength(isComplete?.length);
 
     //set complete todos
-    const isInProgress = data?.filter((item) => item.complate === true);
     setInProgressLength(data?.length - isComplete?.length);
 
     //set complete todos
@@ -26,22 +26,31 @@ const Dashboard = () => {
 
   return (
     <Layout>
-      <div className="2xl:max-w-[1440px] mx-auto flex flex-col lg:items-center gap-[24px]">
-        <div className="grid grid-cols-2 gap-y-[16px] lg:flex lg:gap-[24px]">
-          <InfoBlock title="All Tasks" number={data?.length} />
-          <InfoBlock title="In Progress" number={inProgressLength} />
-          <InfoBlock title="Stuck" number={2} />
-          <InfoBlock title="Done" number={completeLength} />
-        </div>
+      {isLoading ? (
+        <span className="absolute bottom-1/2 left-1/2">
+          <Spinner />
+        </span>
+      ) : (
+        <div className="2xl:max-w-[1440px] mx-auto flex flex-col lg:items-center gap-[24px]">
+          <div className="grid grid-cols-2 gap-y-[16px] lg:flex lg:gap-[24px]">
+            <InfoBlock title="All Tasks" number={data?.length} />
+            <InfoBlock
+              title="In Progress"
+              number={inProgressLength ? inProgressLength : 0}
+            />
+            <InfoBlock title="Stuck" number={2} />
+            <InfoBlock title="Done" number={completeLength} />
+          </div>
 
-        <div className="lg:w-[1080px]">
-          <PieChart
-            completedTodos={completeLength}
-            inProgessTodos={inProgressLength}
-            importantTodos={importantLength}
-          />
+          <div className=" xl:w-[1080px]">
+            <PieChart
+              completedTodos={completeLength}
+              inProgessTodos={inProgressLength}
+              importantTodos={importantLength}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </Layout>
   );
 };
