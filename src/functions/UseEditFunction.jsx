76017f89608f4b39f547ee4f99bd199111d99f2a
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-04 14:03:44 +0400

    update edit function

--- src/functions/UseEditFunction.jsx
+++ src/functions/UseEditFunction.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { supabase } from '../supabaseConfig'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { toastError } from '../components/ToastError'
+import { notifySuccess } from '../components/ToastSuccess'
+import { setTodoTextForEdit } from '../redux/todoEditSlicer'
 
 const UseEditFunction = () => {
+    const dispatch = useDispatch()
     //todo id from redux
     const taskIdForEdit = useSelector(state => state.todoEditStore)
 
@@ -16,10 +20,11 @@ const UseEditFunction = () => {
             
         if (error) {
             console.log(error);
-
+            toastError("Can't Edit Todo")
         } else {
-            console.log(data);
+            notifySuccess("Todo edited successfully")
             refetch()
+            dispatch(setTodoTextForEdit({value: '', id: null}))
         }
     }
 
