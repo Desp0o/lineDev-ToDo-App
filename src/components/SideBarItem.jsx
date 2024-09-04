1151f7f4b1baf6d83b7b5a6ef14cbf39147225b4
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-04 16:25:12 +0400

    create sidebar items

---
+++ src/components/SideBarItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const SideBarItem = ({path, icon, title}) => {
+  return (
+    <Link to={path} className='w-[252px] h-[48px] rounded-[4px] flex items-center pl-[16px] gap-[12px] hover:bg-[#E7E8EA] transition-all'>
+        {icon}
+        <p className='text-[14px] leading-[16px]'>{title}</p>
+    </Link>
+  )
+}
+
+export default SideBarItem
