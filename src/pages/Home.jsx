ee5441d3605ccbdb150e0772f76fbd99e94707e2
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-04 21:56:59 +0400

    home page masonry layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,50 @@
-import React, { useEffect } from 'react'
-import Layout from '../components/Layout'
-import ToDoCard from '../components/ToDoCard'
-import { DateFormatter } from '../components/DateFormatter'
-import AddTask from '../components/AddTask'
-import Spinner from '../components/Spinner'
-import FetchData from '../functions/FetchData'
+import React, { useEffect } from "react";
+import Layout from "../components/Layout";
+import ToDoCard from "../components/ToDoCard";
+import { DateFormatter } from "../components/DateFormatter";
+import AddTask from "../components/AddTask";
+import Spinner from "../components/Spinner";
+import FetchData from "../functions/FetchData";
 
 const Home = () => {
-
-  const { data, isLoading, refetch} = FetchData()
-
+  const { data, isLoading, refetch } = FetchData();
   return (
     <Layout>
       <AddTask refetch={refetch} />
-      <div className='grid lg:grid-cols-4 gap-[30px] grid-flow-dense mt-[56px]'>
-        {isLoading ? <Spinner />
-          : (
-            data && data.length > 0 ? data.map((toDo, index) => {
+      <div className="flex justify-center mt-[56px]">
+        <div className="md:columns-2 lg:columns-3 1xl:columns-4 break-inside-avoid space-y-[23px] gap-[23px] justify-items-center">
+          {isLoading ? (
+            <Spinner />
+          ) : data && data.length > 0 ? (
+            data.map((toDo, index) => {
               const rand = Math.floor(Math.random() * 4) + 1;
               return (
-                <ToDoCard
-                  key={index}
-                  index={index}
-                  text={toDo.description}
-                  bg={rand}
-                  date={<DateFormatter dateProp={toDo.created_at} />}
-                  taskId={toDo.id}
-                  complete={toDo.complate}
-                  important={toDo.important}
-                  refetch={refetch}
-                />
-              )
-            }) : <p className='text-[20px] font-[900] text-center absolute top-1/2 left-1/2'>You Have no ToDos</p>
+                <div className="break-inside-avoid">
+                  <ToDoCard
+                    key={index}
+                    index={index}
+                    text={toDo.description}
+                    bg={rand}
+                    date={<DateFormatter dateProp={toDo.created_at} />}
+                    taskId={toDo.id}
+                    complete={toDo.complate}
+                    important={toDo.important}
+                    refetch={refetch}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <p className="text-[20px] font-[900] text-center absolute top-1/2 left-1/2">
+              You Have no ToDos
+            </p>
           )}
 
-        {/* for padding bottom */}
+          {/* for padding bottom */}
+        </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
