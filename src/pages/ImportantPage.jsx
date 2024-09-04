e14ce1c2cf816795d9afc45e52a47283ca9f6a5d
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-04 22:05:03 +0400

    important page layout

--- src/pages/ImportantPage.jsx
+++ src/pages/ImportantPage.jsx
@@ -3,6 +3,9 @@ import Layout from "../components/Layout";
 import { useUser } from "@clerk/clerk-react";
 import { supabase } from "../supabaseConfig";
 import { useQuery } from "react-query";
+import Spinner from "../components/Spinner";
+import ToDoCard from "../components/ToDoCard";
+import { DateFormatter } from "../components/DateFormatter";
 
 const ImportantPage = () => {
   const { user } = useUser();
@@ -30,11 +33,41 @@ const ImportantPage = () => {
     }
   );
 
-  if (data) {
-    console.log(data);
-  }
+  return (
+    <Layout>
+      <div className="flex justify-center mt-[32px]">
+        <div className="md:columns-2 lg:columns-3 1xl:columns-4 break-inside-avoid space-y-[23px] gap-[23px] justify-items-center">
+          {isLoading ? (
+            <Spinner />
+          ) : data && data.length > 0 ? (
+            data.map((toDo, index) => {
+              const rand = Math.floor(Math.random() * 4) + 1;
+              return (
+                <div className="break-inside-avoid">
+                  <ToDoCard
+                    key={index}
+                    index={index}
+                    text={toDo.description}
+                    bg={rand}
+                    date={<DateFormatter dateProp={toDo.created_at} />}
+                    taskId={toDo.id}
+                    complete={toDo.complate}
+                    important={toDo.important}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <p className="text-[20px] font-[900] text-center absolute top-1/2 left-1/2">
+              You Have no important ToDos
+            </p>
+          )}
 
-  return <Layout></Layout>;
+          {/* for padding bottom */}
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default ImportantPage;
