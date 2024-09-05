6954280f9cae89a58ca6e783737f83d0e7f5c90f
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-05 11:40:01 +0400

    refractor code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "../components/Layout";
-import ToDoCard from "../components/ToDoCard";
-import { DateFormatter } from "../components/DateFormatter";
-import AddTask from "../components/AddTask";
-import Spinner from "../components/Spinner";
 import FetchData from "../functions/FetchData";
-import { useSelector } from "react-redux";
+import MappingMasonryLayout from "../components/MappingMasonryLayout";
 
 const Home = () => {
   const { data, isLoading, refetch } = FetchData();
 
-  const isEditing = useSelector(state => state.todoEditStore)
-
-  useEffect(() => {
-    if (isEditing && isEditing.value.length !== 0) {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-    console.log(isEditing.value);
-    
-  }, [isEditing.value]);
-
-
   return (
     <Layout>
-      <AddTask refetch={refetch} />
-      <div className="flex justify-center mt-[56px]">
-        <div className="md:columns-2 lg:columns-3 1xl:columns-4 break-inside-avoid space-y-[23px] gap-[23px] justify-items-center ">
-          {isLoading ? (
-            <Spinner />
-          ) : data && data.length > 0 ? (
-            data.map((toDo, index) => {
-              const rand = Math.floor(Math.random() * 4) + 1;
-              return (
-                <div className="break-inside-avoid" key={index}>
-                  <ToDoCard
-                    index={index}
-                    text={toDo.description}
-                    bg={rand}
-                    date={<DateFormatter dateProp={toDo.created_at} />}
-                    taskId={toDo.id}
-                    complete={toDo.complate}
-                    important={toDo.important}
-                    refetch={refetch}
-                  />
-                </div>
-              );
-            })
-          ) : (
-            <p className="text-[20px] font-[900] text-center absolute top-1/2 left-1/2">
-              You Have no ToDos
-            </p>
-          )}
-
-          {/* for padding bottom */}
-        </div>
-      </div>
+      <MappingMasonryLayout 
+        data={data} 
+        isLoading={isLoading} 
+        refetch={refetch}
+      />
     </Layout>
   );
 };
--- src/pages/ImportantPage.jsx
+++ src/pages/ImportantPage.jsx
@@ -1,12 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "../components/Layout";
 import { useUser } from "@clerk/clerk-react";
 import { supabase } from "../supabaseConfig";
 import { useQuery } from "react-query";
-import Spinner from "../components/Spinner";
-import ToDoCard from "../components/ToDoCard";
-import { DateFormatter } from "../components/DateFormatter";
-
+import MappingMasonryLayout from "../components/MappingMasonryLayout";
 
 const ImportantPage = () => {
   const { user } = useUser();
@@ -17,7 +14,7 @@ const ImportantPage = () => {
       .select("*")
       .eq("user_id", user.id)
       .eq("important", true)
-      .order("created_at", {ascending: false})
+      .order("created_at", { ascending: false });
 
     if (error) {
       console.log(error);
@@ -37,36 +34,7 @@ const ImportantPage = () => {
 
   return (
     <Layout>
-      <div className="flex justify-center mt-[32px]">
-        <div className="md:columns-2 lg:columns-3 1xl:columns-4 break-inside-avoid space-y-[23px] gap-[23px] justify-items-center">
-          {isLoading ? (
-            <Spinner />
-          ) : data && data.length > 0 ? (
-            data.map((toDo, index) => {
-              const rand = Math.floor(Math.random() * 4) + 1;
-              return (
-                <div className="break-inside-avoid" key={index}>
-                  <ToDoCard
-                    index={index}
-                    text={toDo.description}
-                    bg={rand}
-                    date={<DateFormatter dateProp={toDo.created_at} />}
-                    taskId={toDo.id}
-                    complete={toDo.complate}
-                    important={toDo.important}
-                  />
-                </div>
-              );
-            })
-          ) : (
-            <p className="text-[20px] font-[900] text-center absolute top-1/2 left-1/2">
-              You Have no important ToDos
-            </p>
-          )}
-
-          {/* for padding bottom */}
-        </div>
-      </div>
+      <MappingMasonryLayout data={data} isLoading={isLoading} />
     </Layout>
   );
 };
